7e3077a9cc421c4f13b39059e25ae7aa66436ef8
Lorenzo Vannucci <lore.ucci@gmail.com>
2023-07-08 12:51:10 +0200

    Rays and camera

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,14 +4,34 @@ use std::io::Write;
 
 extern crate rusty_rays;
 use rusty_rays::color::{write_color, Color};
+use rusty_rays::ray::Ray;
+use rusty_rays::vec3::{unit_vector, Point3, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = unit_vector(&r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+}
 
 fn main() {
     // image
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio).trunc() as i32;
 
     // file
     let mut f = BufWriter::new(File::create("output.ppm").expect("Unable to create file"));
+
+    // camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // render
     writeln!(f, "P3").unwrap();
@@ -21,12 +41,10 @@ fn main() {
     for j in (0..image_height).rev() {
         println!("scanlines remaining: {j}");
         for i in 0..image_width {
-            let pixel_color = Color::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
-            );
-
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+            let pixel_color = ray_color(&r);
             write_color(&mut f, &pixel_color).unwrap();
         }
     }
